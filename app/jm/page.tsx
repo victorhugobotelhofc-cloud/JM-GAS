03d07af6cbd5277ab5ced2bdf9282e066cb4a607
victorhugobotelhofc-cloud <victorhugobotelhofc@gmail.com>
2026-08-17 15:57:18 -0300

    Update page.tsx

--- app/jm/page.tsx
+++ app/jm/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import { supabase } from "@/lib/supabase";
 
 type Pedido = {
